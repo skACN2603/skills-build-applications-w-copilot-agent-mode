4cd924c74fd2ebf9b19668bda17b7f4ffb1108e8
skACN2603 <sandeep.dr.kumar@accenture.com>
2025-12-01 10:59:04 +0000

    front end changes

---
+++ octofit-tracker/frontend/src/components/OctofitLogo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from '../../docs/octofitapp-small.png';
+
+const OctofitLogo = () => (
+  <img src={logo} alt="Octofit Logo" className="octofit-logo" />
+);
+
+export default OctofitLogo;
